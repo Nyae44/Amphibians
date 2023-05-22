313e8da828f5c6d593fa98fc29356492e4d5ced6
daley <tingadaley@gmail.com>
2023-05-22 18:54:57 +0300

    Created a lazyColumn to display the API response

--- app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -1,13 +1,18 @@
 package com.example.amphibians.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,6 +21,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -74,23 +81,54 @@ fun ErrorScreen(modifier: Modifier = Modifier){
     }
 }
 @Composable
-fun AmphibiansPhotoCard(photo: AmphibiansPhoto, modifier: Modifier = Modifier){
-    AsyncImage(
-        model = ImageRequest.Builder(context = LocalContext.current)
-            .data(photo.imgSrc)
-            .crossfade(true)
-            .build(),
-        error = painterResource(id = R.drawable.ic_broken_image),
-        placeholder = painterResource(id = R.drawable.loading_img),
-        contentDescription = stringResource(id = R.string.amphibian_photo),
-        contentScale = ContentScale.FillBounds
-    )
+fun AmphibiansPhotoCard(photo: AmphibiansPhoto, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp)
+
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = photo.name,
+                modifier = modifier
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start
+                )
+            Text(
+                text = photo.type,
+                modifier = modifier
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start
+            )
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(photo.imgSrc)
+                    .crossfade(true)
+                    .build(),
+                error = painterResource(id = R.drawable.ic_broken_image),
+                placeholder = painterResource(id = R.drawable.loading_img),
+                contentDescription = stringResource(id = R.string.amphibian_photo),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = photo.description,
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
 }
 @Composable
 fun PhotosGridScreen(photos: List<AmphibiansPhoto>, modifier: Modifier = Modifier) {
         LazyColumn(
+
             modifier = modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(4.dp)
+            contentPadding = PaddingValues(4.dp),
+            verticalArrangement = Arrangement.spacedBy(21.dp)
         ) {
             itemsIndexed(photos) { index, photo ->
                 AmphibiansPhotoCard(photo = photo)
